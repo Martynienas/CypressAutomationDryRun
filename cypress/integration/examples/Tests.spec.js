64c0904a9fc7c0fb62cd7d9ab6b4b4685a01dced
Martynas_Luneckas <Martynas.Luneckas@devbridge.com>
2020-06-10 22:44:47 +0300

    Added test to search for pandas

--- cypress/integration/examples/Tests.spec.js
+++ cypress/integration/examples/Tests.spec.js
@@ -4,3 +4,12 @@ it("Loads the duckduckgo website", () => {
     cy.visit("www.duckduckgo.com")
     cy.contains("Tired of being tracked online? We can help.");
 });
+
+it("should cintain search criteria in search results title", () => {
+    cy.visit("www.duckduckgo.com")
+    cy.get('#search_form_homepage').type("intitle:panda")
+    cy.get('#search_button_homepage').click();
+    cy.get('.result__title').each(($item) => { cy.get($item).contains('Panda', { matchCase: false }) })
+})
+
+
